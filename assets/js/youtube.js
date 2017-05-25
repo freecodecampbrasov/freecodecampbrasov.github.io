e49d1ec433e8b9be730d4d2c53fe90762fb88973
praph <praphweb@gmail.com>
2017-05-25 15:03:25 +0300

    added JS clips

---
+++ assets/js/youtube.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('.youtube-playlist a').each(function(){
+        var element = $(this);
+
+        element.click(function(e){
+            var iframe = $('.youtube-iframe-container');
+
+            iframe.html('<iframe width="560" height="315" src="https://www.youtube.com/embed/'+element.data('youtubeId')+'?ecver=1&autoplay=1" frameborder="0" allowfullscreen></iframe>');
+
+            $('.youtube-playlist a').removeClass('active');
+            element.addClass('active');
+            e.preventDefault();
+        });
+    })
+})
