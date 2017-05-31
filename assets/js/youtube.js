f9323f838dd9b7cb5a8e41310269c5506eb62fd7
praph <praphweb@gmail.com>
2017-05-31 20:02:05 +0300

    responsiveness updatess

--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -5,7 +5,7 @@ $(function(){
         element.click(function(e){
             var iframe = $('.youtube-iframe-container');
 
-            iframe.html('<iframe width="560" height="315" src="https://www.youtube.com/embed/'+element.data('youtubeId')+'?ecver=1&autoplay=1" frameborder="0" allowfullscreen></iframe>');
+            iframe.html('<iframe width="100%" height="315" src="https://www.youtube.com/embed/'+element.data('youtubeId')+'?ecver=1&autoplay=1" frameborder="0" allowfullscreen></iframe>');
 
             $('.youtube-playlist a').removeClass('active');
             element.addClass('active');
